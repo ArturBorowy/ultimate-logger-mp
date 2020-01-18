bb535244bb84da187e3866f398df4f6f8b7933eb
Artur Borowy <artur.borowy843@gmail.com>
2020-01-18 19:58:37 +0100

    Fix TagDataConverter throwing exception when fileName is null.

--- src/main/java/com/ultimatelogger/multiplatform/tag/dataprovider/TagDataConverter.kt
+++ src/main/java/com/ultimatelogger/multiplatform/tag/dataprovider/TagDataConverter.kt
@@ -2,8 +2,12 @@ package com.ultimatelogger.multiplatform.tag.dataprovider
 
 internal class TagDataConverter {
 
+    companion object {
+        private const val FILE_NAME_MISSING = "UnknownFile"
+    }
+
     fun fromStackTraceElement(stackTraceElement: StackTraceElement) =
-            TagData(stackTraceElement.fileName,
+            TagData(stackTraceElement.fileName ?: FILE_NAME_MISSING,
                     stackTraceElement.className,
                     stackTraceElement.methodName,
                     stackTraceElement.lineNumber.toString())
